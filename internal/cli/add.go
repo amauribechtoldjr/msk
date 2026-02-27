2f5b0dfecb742ebf2263b8cde88e1d9fe50b5d38
Amauri Bechtold Junior <amauribechtoldjr@gmail.com>
2026-02-26 21:28:53 -0300

    feat: add pass generator

--- internal/cli/add.go
+++ internal/cli/add.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 	"fmt"
 
+	clip "github.com/amauribechtoldjr/msk/internal/clip"
+	"github.com/amauribechtoldjr/msk/internal/generator"
 	"github.com/amauribechtoldjr/msk/internal/logger"
 	"github.com/amauribechtoldjr/msk/internal/validator"
 	"github.com/amauribechtoldjr/msk/internal/wipe"
@@ -30,9 +32,22 @@ func NewAddCmd(holder *ServiceHolder) *cobra.Command {
 				return fmt.Errorf("invalid password name: %v", err)
 			}
 
-			password, err := PromptSafeValue("Enter password:")
-			if err != nil {
-				return err
+			generate, _ := cmd.Flags().GetBool("generate")
+			length, _ := cmd.Flags().GetInt("length")
+			noSymbols, _ := cmd.Flags().GetBool("no-symbols")
+
+			var password []byte
+
+			if generate {
+				password, err = generator.GeneratePassword(length, noSymbols)
+				if err != nil {
+					return fmt.Errorf("failed to generate password: %w", err)
+				}
+			} else {
+				password, err = PromptSafeValue("Enter password:")
+				if err != nil {
+					return err
+				}
 			}
 			defer wipe.Bytes(password)
 
@@ -41,10 +56,32 @@ func NewAddCmd(holder *ServiceHolder) *cobra.Command {
 				return fmt.Errorf("failed to add secret: %w", err)
 			}
 
-			logger.PrintSuccess("Password added successfully\n")
+			secret, err := holder.Service.GetSecret(name)
+			if err != nil {
+				return fmt.Errorf("failed to add secret: %w", err)
+			}
+			defer wipe.Bytes(secret)
+
+			if generate {
+				err = clip.CopyText(secret)
+				if err != nil {
+					return fmt.Errorf("failed to copy password to your clipboard: %w", err)
+				}
+
+				logger.PrintSuccess("Password generated and copied to clipboard (press Ctrl+V to paste)\n\n")
+
+				clip.Clear()
+			} else {
+				logger.PrintSuccess("Password added successfully\n")
+			}
+
 			return nil
 		},
 	}
+
+	addCmd.Flags().BoolP("generate", "g", false, "Generate a random password instead of prompting")
+	addCmd.Flags().IntP("length", "l", 16, "Length of the generated password")
+	addCmd.Flags().Bool("no-symbols", false, "Exclude symbols from the generated password")
 
 	return addCmd
 }
